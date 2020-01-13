16e2a5a33ec451bb343ed6d9a5d5c254139a60c6
Admin <admin@admins-macbook-air.home>
2020-01-13 22:35:02 +0100

    Updates

---
+++ precache-manifest.b2b66472aadf9043f515c67c7fac5085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12954fa03f2e3da59d7f849640e7c7fb",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "15215a996e423c6b91dd",
+    "url": "/Tic-Tac-Toe/static/css/main.e6bb8bdc.chunk.css"
+  },
+  {
+    "revision": "9bcc8c8b373c4b556edc",
+    "url": "/Tic-Tac-Toe/static/js/2.560fdbda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tic-Tac-Toe/static/js/2.560fdbda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15215a996e423c6b91dd",
+    "url": "/Tic-Tac-Toe/static/js/main.f5a68c44.chunk.js"
+  },
+  {
+    "revision": "84440446c7b387ff9fec",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.e4849b93.js"
+  }
+]);
